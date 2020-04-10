aeda5b8634ccdeea3d38203dba6f9f5c4a7bfcc0
emi <emazzurque@gmail.com>
2020-04-10 02:32:57 -0300

    como navegar entre pestañas

--- ejerciciotres.py
+++ ejerciciotres.py
@@ -13,9 +13,9 @@ class usando_unittest(unittest.TestCase):
     def test_buscar(self):#importante arrancar con test por que la va a venira buscar aca
         driver = self.driver
         driver.get("http://www.google.com")
-        self.assertIn("Google",driver.title)
-        elemento = driver.find_element_by_name("q")
-        elemento.send_keys("selenium")
+        self.assertIn("Google",driver.title)# busca si en el titulo se encuentra la palabra google
+        elemento = driver.find_element_by_name("q")#busca el name= q (buscador)
+        elemento.send_keys("selenium")#en q escribe selenium
         elemento.send_keys(Keys.RETURN)
         time.sleep(5)
         assert "No se encontro el elemento" not in driver.page_source
