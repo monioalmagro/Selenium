eeb6f76eea897e6fea64b0a0956a5ebe4e728e24
emi <emazzurque@gmail.com>
2020-04-10 16:38:30 -0300

    IMPLICIT WAIT

---
+++ ejercicio8.py
@@ -0,0 +1,21 @@
+#IMPLICIT WAIT
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+
+
+class usando_unittest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path = "/home/almagro/Escritorio/Selenium/chromedriver")
+
+    def test_usando_implicit_wait(self):
+        driver = self.driver 
+        driver.implicitly_wait(5)#segundos
+        driver.get("http:/www.google.com")
+        myDynamicElement = driver.find_element_by_name("q")
+
+if __name__ == "__main__":
+    unittest.main()        
