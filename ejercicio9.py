5d65c9dcb05d2969ac0c0e8d8586506ecae498d0
emi <emazzurque@gmail.com>
2020-04-10 18:42:16 -0300

    comparing images with OpenCV

---
+++ ejercicio9.py
@@ -0,0 +1,50 @@
+# comparacion de imagenes con OPEN CV
+# SCREENSHOTSELENIUM
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+import cv2
+import time
+
+
+class usando_unittest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path = "/home/almagro/Escritorio/Selenium/chromedriver")
+        
+
+    def test_usando_opencv(self):
+        driver = self.driver
+        
+        #driver.set_window_position(0, 0)
+        #driver.set_window_size(1366, 768)   # problema con tamanos de las imagenes"""
+        driver.get("https://www.google.com/")
+        driver.save_screenshot('img2.png')
+        time.sleep(3)
+
+    def test_comparando_imagenes(self):
+        img1 = cv2.imread('img1.png')
+        img2 = cv2.imread('img2.png')
+
+        diferencia = cv2.absdiff(img1,img2)
+        imagen_gris = cv2.cvtColor(diferencia,cv2.COLOR_BGR2GRAY)
+        contours,_ = cv2.findContours(imagen_gris,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+        for c in contours:
+            if cv2.contourArea(c) >=20:
+                posicion_x,posicion_y,ancho,alto = cv2.boundingRect(c)
+                cv2.rectangle(img1,(posicion_x,posicion_y),(posicion_x+ancho,posicion_y+alto),(0,0,255),2)
+
+        while(1):
+            cv2.imshow('Imagen1',img1)
+            cv2.imshow('Imagen2',img2)
+            cv2.imshow('Diferencias detectadas',diferencia)
+            teclado = cv2.waitKey(5) & 0xFF
+            if teclado == 27:
+                break
+            cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    unittest.main()               
