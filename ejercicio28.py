4d2bd357644a34d848cff1f0363fc32bb401f9a4
emi <emazzurque@gmail.com>
2020-05-20 20:29:42 -0300

    28

--- ejercicio28.py
+++ ejercicio28.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path = "/home/almagro/Escritorio/Selenium/chromedriver")
-driver.get("http://almagro.pythonanywhere.com/")
+driver.get("http://almagro.pythonanywhere.com/porfolio/")
 driver.get_screenshot_as_file('/home/almagro/Escritorio/Selenium/screen.png')
+driver.close()
